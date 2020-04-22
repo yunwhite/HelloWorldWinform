49062a7527c75cd7b397aa6912d7d211ed5a726e
yunwhite <s2018s12@e-mirim.hs.kr>
2020-04-22 09:07:02 +0900

    버튼 추가

--- HelloWorldWinform/Form1.Designer.cs
+++ HelloWorldWinform/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(289, 183);
             this.button1.TabIndex = 0;
-            this.button1.Text = "이거 누르면 롤 플레";
+            this.button1.Text = "이거 누르면 10만원";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
